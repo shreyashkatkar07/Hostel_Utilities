a47f2ec50039b0db9fb54f5aa64eb1eed9dbee46
hrx554 <harshrange123@gmail.com>
2023-11-11 21:51:59 +0530

    role based routing done

--- client/src/Admin/LeaveApproval.jsx
+++ client/src/Admin/LeaveApproval.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import homeimg from "./../../assets/svg/Home-white.png";
+import homeimg from "./../assets/svg/Home-white.png";
 import { Link } from "react-router-dom";
 
 const LeaveApproval = () => {
---
+++ client/src/Admin/StudentRecords.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StudentRecords = () => {
+  return (
+    <>
+      <div className="md:w-[85vw] w-[100%] sticky left-[20vw] bg-gray-800 text-white lg:p-8 p-1">Students Records</div>
+    </>
+  );
+};
+
+export default StudentRecords;
